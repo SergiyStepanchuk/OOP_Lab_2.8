bb3faa15711cdaaf0e50e26993a3d617034170a1
SergiyStepanchuk <prochliste@gmail.com>
2021-05-10 12:44:43 +0300

    init

---
+++ OOP_Lab_2.8/UnitTest/UnitTest.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/Bill.h"
+#include "../Program/Bill.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest17)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Bill B1;
+			B1.SetSurname("Stepanchuk");
+			Assert::AreEqual("Stepanchuk", B1.GetSurname().c_str());
+		}
+	};
+}
